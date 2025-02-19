618fa46f12ddc1434af01e7a5accdcb90aec2fe2
Richa251188 <richa251188@gmail.com>
2025-02-19 16:16:37 +0530

    Add files via upload
    Initial commit

---
+++ SampleTaskTest/ViewModel/CitiesViewModel.swift
@@ -0,0 +1,25 @@
+import Foundation
+
+class CitiesViewModel: ObservableObject {
+    @Published var citiesList: [CitiesModel] = []
+    
+    func getCity() {
+        if let path = Bundle.main.path(forResource: "au_cities", ofType: "json"){
+            let url = URL(fileURLWithPath: path)
+            
+            do {
+                let jsondata = try Data(contentsOf: url)
+                let cities = try JSONDecoder().decode([CitiesModel].self, from: jsondata)
+                citiesList = cities
+                citiesList.sort(by: { $0.city < $1.city })
+                
+            } catch {
+                print("error")
+            }
+            
+        } else {
+            print("file not found")
+        }
+    }
+}
+
